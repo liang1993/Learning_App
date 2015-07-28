7169ab075b0751d4c1daa35f58f169e34735b959
liang1993 <1027622302@qq.com>
2015-07-28 19:06:11 +0800

    使用了RecyclerView代替了之前的ListView

--- app/src/main/java/com/app/l/learningapp/activities/SwipeRefreshAct.java
+++ app/src/main/java/com/app/l/learningapp/activities/SwipeRefreshAct.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.app.l.learningapp.R;
 
-import java.util.ArrayList;
 
 /**
  * Created by liang on 15/7/28.
@@ -19,9 +22,12 @@ public class SwipeRefreshAct extends Activity implements SwipeRefreshLayout.OnRe
 
     private SwipeRefreshLayout swipeRefreshLayout;
 
-    private ListView listView;
+    private RecyclerView mRecyclerView;
 
-    private ArrayList<String> list = new ArrayList<>();
+    private RecyclerView.Adapter mAdapter;
+
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,19 +40,20 @@ public class SwipeRefreshAct extends Activity implements SwipeRefreshLayout.OnRe
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
 
-        listView = (ListView) findViewById(R.id.list);
-        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getData()));
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
-    }
+        //use this setting to improve performance if you know that changes in content do noe change
+        //the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
 
-    private ArrayList<String> getData() {
-        list.add("hello1");
-        list.add("hello2");
-        list.add("hello3");
-        list.add("hello4");
-        list.add("hello5");
-        list.add("hello6");
-        return list;
+        //use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //specify an adapter
+        mAdapter = new RecyclerAdapter(new String[]{"1", "2", "3"});
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 
     @Override
@@ -57,6 +64,52 @@ public class SwipeRefreshAct extends Activity implements SwipeRefreshLayout.OnRe
                 swipeRefreshLayout.setRefreshing(false);
             }
         }, 5000);
+    }
+
+}
+
+class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+    private String[] mDataSet;
+
+    //provide a reference to the views for each data item
+    //complex data items may need more than one view per item ,and
+    //you provide access to all the views for a data item in a view holder
+
+    //create new views(invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        //create a new view
+        View v  = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.my_list_view_item,viewGroup,false);
+
+        //set the view's size ,margins ,padding and layout params
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        viewHolder.mTextView.setText(mDataSet[i]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        //each data item is just a string in this case
+
+        public TextView mTextView;
+
+        public ViewHolder(View v) {
+            super(v);
+            mTextView = (TextView)v.findViewById(R.id.text_view);
+
+        }
+    }
+
+    public RecyclerAdapter(String[] myDataSet) {
+        mDataSet = myDataSet;
     }
 
 }
