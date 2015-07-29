0d72c342d0fb5479f6a7f728f0866a1cb779f26a
liang1993 <1027622302@qq.com>
2015-07-29 16:51:39 +0800

    在首页添加了toolbar和drawerlayout的demo使用

--- app/src/main/java/com/app/l/learningapp/LauncherAct.java
+++ app/src/main/java/com/app/l/learningapp/LauncherAct.java
@@ -1,10 +1,16 @@
 package com.app.l.learningapp;
 
-import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.app.l.learningapp.activities.CustomViewAct;
 import com.app.l.learningapp.activities.SwipeRefreshAct;
@@ -12,7 +18,7 @@ import com.app.l.learningapp.activities.SwipeRefreshAct;
 /**
  * Created by liang on 15/7/28.
  */
-public class LauncherAct extends Activity implements View.OnClickListener {
+public class LauncherAct extends AppCompatActivity implements View.OnClickListener {
 
     private Button swipeButton, countViewButton, titleViewButton,listViewButton;
 
@@ -20,10 +26,46 @@ public class LauncherAct extends Activity implements View.OnClickListener {
 
     private final String INTENTTAG = "layout";
 
+    private Toolbar toolbar;
+
+    private DrawerLayout mDrawerLayout;
+
+    private ListView listView;
+
+    private String[] lvs = {"List Item 01", "List Item 02", "List Item 03", "List Item 04"};
+
+    private ArrayAdapter arrayAdapter;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_actlayout);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.dl_left);
+        listView = (ListView)findViewById(R.id.lv_left_menu);
+        toolbar.setTitle("ToolBar");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);//返回键可用
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, lvs);
+        listView.setAdapter(arrayAdapter);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.draw_open, R.string.draw_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
         swipeButton = (Button) findViewById(R.id.swipe_refresh);
         swipeButton.setOnClickListener(this);
@@ -36,6 +78,8 @@ public class LauncherAct extends Activity implements View.OnClickListener {
 
         listViewButton = (Button)findViewById(R.id.my_list_view);
         listViewButton.setOnClickListener(this);
+
+
     }
 
     @Override
